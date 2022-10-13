73407ad9d7eaafdf18adfe4e1ad20127c2994c56
Adam Bruce <adam.bruce@Adams-MacBook-Air.local>
2022-10-13 10:06:00 -0500

    Print Statements for game instruction

--- Bodnar_Bruce_FinalProject.py
+++ Bodnar_Bruce_FinalProject.py
@@ -2,6 +2,16 @@
 # Lilia Bodnar & Adam Bruce
 # WHRTLUHC
 
+def wordleGame():
+    print("Welcome to our Wordle game! Try and find the five letter word before running out of guesses.")
+    print("")
+    print("Levels: ")
+    print("1: Easy (8 guesses)")
+    print("2: Medium (6 guesses)")
+    print("3: Hard (4 guesses)")
+    print("")
+    print("Enter the number corresponding with the number of guesses you would like: ")
+    guessLevel = input()
 
 
 
