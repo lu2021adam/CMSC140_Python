9467aaa81d997eff01220c30523a54996c95a9be
Adam Bruce <adam.bruce@Adams-MacBook-Air.local>
2022-10-13 09:29:44 -0500

    Added Github usage information.

--- tasks.py
+++ tasks.py
@@ -6,3 +6,19 @@
 #   print out/determine results
 # function that asks if they want to play again
 #   call the wordle game function if they enter yes to play again, else terminate
+
+#### USING GITHUB
+
+# An understanding of how to use the Git Hub repository
+
+# git init
+# git branch -m main
+# copy code from git hub
+# git remote add origin "git link"
+# git pull origin main
+# git add . (EVERY TIME NEW INFORMATION)
+# git commit -m "Message Here"
+# git push origin main
+
+# Pull to get from git hub
+# Push to get to git hub
