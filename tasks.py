dbaca101494f97df7af48faf57aeefbfa621e0d7
bodnarlil <lilia.z.bodnar@lawrence.edu>
2022-10-13 09:23:32 -0500

    Project Tasks

---
+++ tasks.py
@@ -0,0 +1,8 @@
+# tasks:
+# print out instructions
+# function that runs the wordle game itself
+#   select the random word
+#   write the loop that asks for guesses
+#   print out/determine results
+# function that asks if they want to play again
+#   call the wordle game function if they enter yes to play again, else terminate
