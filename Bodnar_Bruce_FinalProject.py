38d67364ef8a79f4291eacd3505f50c44032d65d
bodnarlil <lilia.z.bodnar@lawrence.edu>
2022-11-13 18:23:19 -0600

    changed while to while not

--- Bodnar_Bruce_FinalProject.py
+++ Bodnar_Bruce_FinalProject.py
@@ -16,7 +16,7 @@ def wordleGame():
     print("")
     print("Enter the number corresponding with the number of guesses you would like: ")
     guessLevel = input()
-
+    guessedWords = []
 
     # run the following while IF guessLevel has a letter and is not 1, 2, or 3
     while(regex.search('[a-zA-Z]', guessLevel) != None or (int(guessLevel) != 1 and int(guessLevel) != 2 and int(guessLevel) != 3)):
@@ -41,7 +41,7 @@ def wordleGame():
         words = file.read()
         seperatedWords = list(map(str, words.split()))
         randomWord = rand.choice(seperatedWords)
-        print(randomWord)
+        print(randomWord) # delete this
 
     # 1. write a loop here that runs for the number of guesses the user had to guess the random word
         # 1.2 write an if/else to tell them if they got it right or wrong or to guess again or to say they lost and print out the word
@@ -59,6 +59,7 @@ def wordleGame():
             print("You still have", guessesLeft, "guesses remaining")
             userGuess = input()
         userGuess = str(userGuess).lower()
+        guessedWords += [userGuess]
         guessesLeft -= 1
         correctLetters = ""
 
@@ -85,15 +86,16 @@ def playWordle():
     yes_regex = regex.compile(r'(Y|y)(es)?$') # a regex for valid forms of yes
     no_regex = regex.compile(r'(N|n)(o)?$') # a regex for valid forms of no
     # while loop checks to see if they entered a valid yes/no and runs until they do; not working right but the program still runs
-    while(bool(regex.match(yes_regex, playAgain)) == False and bool(regex.match(no_regex, playAgain)) == False):
+    while not regex.match(yes_regex, playAgain) and not regex.match(no_regex, playAgain):
         print("Please enter a valid form of yes or no. Valid Forms: Yes, Y, yes, y, No, N, no, n")
         playAgain = input()
 
     # this while loop runs while playAgain is yes and terminates when it is not and also check to see if playAgain is a valid form of yes/no
     while(regex.match(yes_regex, playAgain)):
-        while(bool(regex.match(yes_regex, playAgain)) == False and bool(regex.match(no_regex, playAgain)) == False):
+        while not regex.match(yes_regex, playAgain) and not regex.match(no_regex, playAgain):
             print("Please enter a valid form of yes or no. Valid Forms: Yes, Y, yes, y, No, N, no, n")
-            playAgain = input()
+            playAgain = str(input())
+
         if regex.match(yes_regex, playAgain):
             print("")
             print("---------------------------")
@@ -102,6 +104,9 @@ def playWordle():
             print("")
             print("Do you want to play again? (yes/no)")
             playAgain = str(input())
+            while not regex.match(yes_regex, playAgain) and not regex.match(no_regex, playAgain):
+                print("Please enter a valid form of yes or no. Valid Forms: Yes, Y, yes, y, No, N, no, n")
+                playAgain = str(input())
     # this will run once they say no
     print("Okay! Have a nice day!")
 
